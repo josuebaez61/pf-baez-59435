e55ac6f29413020a794cb6c2d6a13170d248a253
josue baez <josuebaez61@gmail.com>
2024-10-21 21:16:25 -0300

    ADD, authUser$

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AuthData } from '../../features/auth/models';
-import { Observable, of, throwError } from 'rxjs';
+import { BehaviorSubject, Observable, of, throwError } from 'rxjs';
 import { User } from '../../features/dashboard/users/models';
 import { generateRandomString } from '../../shared/utils';
+import { Router } from '@angular/router';
 
 const FAKE_USER: User = {
   email: 'admin@mail.com',
@@ -15,10 +16,23 @@ const FAKE_USER: User = {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  private _authUser$ = new BehaviorSubject<null | User>(null);
+  public authUser$ = this._authUser$.asObservable();
+
+  constructor(private router: Router) {}
+
   login(data: AuthData): Observable<User> {
     if (data.email != FAKE_USER.email || data.password != FAKE_USER.password) {
       return throwError(() => new Error('Los datos son invalidos'));
     }
+
+    this._authUser$.next(FAKE_USER);
+
     return of(FAKE_USER);
+  }
+
+  logout() {
+    this._authUser$.next(null);
+    this.router.navigate(['auth', 'login']);
   }
 }
--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from './users/models';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +12,13 @@ import { Router } from '@angular/router';
 export class DashboardComponent {
   showFiller = false;
 
-  constructor(private router: Router) {}
+  authUser$: Observable<User | null>;
+
+  constructor(private router: Router, private authService: AuthService) {
+    this.authUser$ = this.authService.authUser$;
+  }
 
   logout(): void {
-    localStorage.removeItem('token');
-    this.router.navigate(['auth', 'login']);
+    this.authService.logout();
   }
 }
